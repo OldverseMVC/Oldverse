3f6c7cd990e089aff5721dc12944419eaeb91012
R.G <61545240+Rix565@users.noreply.github.com>
2022-05-29 18:30:22 +0000

    Added this cool page

---
+++ Oldverse-PHP/php/ip-banned.php
@@ -0,0 +1,14 @@
+<?
+require_once "../config.php";
+if(!in_array($_SERVER['REMOTE_ADDR'], FORBIDDEN_IPS)){
+    header("Location: /");
+    exit;
+}
+?>
+<audio controls autoplay hidden>
+    <source src="/assets/brugers.wav" type="audio/mpeg">
+</audio>
+<h1 class="headline">got fcked!!!!1!1!1!!1!1!1!1!1!1!1</h1>
+<div class="no-content">ya got ip bnaned from oldvers >:)))))))))</p></div>
+<p>youll nevzr see dis 😂😂😂</p>
+<a href="/">try again, see eff ur unbanned betch, i dare you</a>
