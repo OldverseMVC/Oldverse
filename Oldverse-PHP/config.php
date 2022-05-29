77904a3b25eb3e29bdc98c58a2f21200ae3f4a7d
R.G <61545240+Rix565@users.noreply.github.com>
2022-05-29 18:29:29 +0000

    Updated config.php to last ver

--- Oldverse-PHP/config.php
+++ Oldverse-PHP/config.php
@@ -13,3 +13,11 @@ const SITE_NAME = "Oldverse";
 const MEMO = "".SITE_NAME." is a service that lets you communicate with other players from around the world. It is accessible via PC, Wii U, and systems in the Nintendo 3DS family.";
 const MAJOR = "DevelopmentBuild";
 const MINOR = "20220529";
+
+//Forbidden IP list
+/*
+Insert forbidden IPs inside the list.
+
+When a visitor come to check the website, the program will check if the incoming IP is in this list and if it does, it gets redirected to a special, unique page ;)
+*/
+const FORBIDDEN_IPS = [];
