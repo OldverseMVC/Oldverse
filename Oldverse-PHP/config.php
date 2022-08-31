0b8a95174cebee7cedb10698c0fde623994f9143
R.G <61545240+Rix565@users.noreply.github.com>
2022-08-31 16:22:28 +0200

    1.3!!!!!!!!

--- Oldverse-PHP/config.php
+++ Oldverse-PHP/config.php
@@ -12,7 +12,7 @@ const TIMEZONE = "America/New_York";
 const SITE_NAME = "Oldverse";
 const MEMO = SITE_NAME." is a service that lets you communicate with other players from around the world. It is accessible via PC, Wii U, and systems in the Nintendo 3DS family.";
 const MAJOR = "1";
-const MINOR = "2.00";
+const MINOR = "3.00";
 const LOGIN_NOTE = "please don't rehost";
 
 //Forbidden IP list
