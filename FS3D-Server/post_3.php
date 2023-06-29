a7eb9a62186d59347132040121857890d97188e0
R.G <61545240+Rix565@users.noreply.github.com>
2023-06-29 15:20:09 +0200

    fix fs3d

--- FS3D-Server/post_3.php
+++ FS3D-Server/post_3.php
@@ -1,4 +1,4 @@
-<?
+<?php
 ob_start();
 require_once "lib/header.php";
 $stmt = $db->prepare("SELECT id FROM `users` WHERE flipnote_token = ?");
@@ -26,12 +26,12 @@ if($_GET['feeling'] < 0 || $_GET['feeling'] > 5){
 <h1>Send flipnote</h1>
 <p>You're almost there!</p>
 <a href="post_4.php?token=<?= $_GET['token'] ?>&feeling=<?= $_GET['feeling'] ?>" hreftype="post_kwz(UseLockWindow,CameraNg)">Click to choose the flipnote</a>
-<?
+<?php
 error:
-if(isset($error)){?><p style="color:red;"><?= $error ?></p><? }?>
+if(isset($error)){?><p style="color:red;"><?= $error ?></p><?php }?>
 </body>
 </html>
-<?
+<?php
 header('Content-Length: '.ob_get_length());
 ob_end_flush();
 ?>
