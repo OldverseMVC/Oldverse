e9557f1158d258fe9fac988398f62c1fbc963a73
R.G <61545240+Rix565@users.noreply.github.com>
2023-06-29 15:19:23 +0200

    fix fs3d

--- FS3D-Server/post.php
+++ FS3D-Server/post.php
@@ -1,4 +1,4 @@
-<?
+<?php
 ob_start();
 require_once "lib/header.php";
 ?>
@@ -6,7 +6,7 @@ require_once "lib/header.php";
     <a hreftype="post_keyboard(10,Type it here,CodeWord,)" href="post_2.php">Personal Flipnote Token (click to type)</a>
 </body>
 </html>
-<?
+<?php
 header('Content-Length: '.ob_get_length());
 ob_end_flush();
 ?>
