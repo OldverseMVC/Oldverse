24f1d13b378eb299232349376d1dac6b110dae9b
R.G <61545240+Rix565@users.noreply.github.com>
2023-06-29 15:20:53 +0200

    why

--- FS3D-Server/info.php
+++ FS3D-Server/info.php
@@ -5,7 +5,7 @@ require_once "lib/header.php";
     <h1>What is this</h1>
     <p>This is an companion app to post posts to Oldverse with an embedded flipnote in it!</p>
     <h3>Why? Like look, there is 100 things more c00ler than that.</h3>
-    <p>Because it is cool to post flips, no? Like you can upload a flipnote about your dog scrathing their ass!</p>
+    <p>Because it is cool to post flips, no? Like you can upload a flipnote about idk, a song/a funny moment!</p>
 </body>
 </html>
 <?php
